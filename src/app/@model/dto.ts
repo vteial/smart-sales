6e0c37be9957392a75db6086bab2bb7cdbe90608
Eialarasu <vteial@gmail.com>
2020-09-07 23:27:29 +0530

    testimonial page implemented

--- src/app/@model/dto.ts
+++ src/app/@model/dto.ts
@@ -1,7 +1,8 @@
+import {prop} from '@rxweb/reactive-form-validators';
+
 export class UserDto {
 
   userId: string;
-
   password: string;
 
   constructor() {  }
@@ -9,4 +10,18 @@ export class UserDto {
   asRequestModel(): any {
     return { user_id: this.userId, password: this.password };
   }
+}
+
+export class Testimonial {
+
+  @prop()
+  id: number;
+  @prop()
+  name: string;
+  @prop()
+  designation: string;
+  @prop()
+  description: string;
+
+  constructor() {  }
 }
--- src/app/testimonial/testimonial.component.ts
+++ src/app/testimonial/testimonial.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ToastrService} from 'ngx-toastr';
-import {BsModalService} from 'ngx-bootstrap/modal';
 import {BaseComponent} from '../@shared/base.component';
 import {ApiService} from '../@shared/api.service';
+import {Testimonial} from '../@model/dto';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {RxFormBuilder, RxwebValidators} from '@rxweb/reactive-form-validators';
 
 @Component({
   selector: 'app-testimonial',
@@ -13,18 +15,68 @@ export class TestimonialComponent extends BaseComponent implements OnInit {
 
   items = [];
 
+  item: Testimonial;
+
+  itemFormGroup: FormGroup;
+
   constructor(private appService: ApiService,
               private toastr: ToastrService,
-              private modalService: BsModalService) {
+              private formBuilder: RxFormBuilder) {
     super();
     super.viewName = 'Testimonial';
+    this.init();
   }
 
   ngOnInit(): void {
     this.appService.fetchTestimonials().subscribe((data: any) => {
       console.log(data);
-      this.items = data;
+      this.copy(data);
+      this.edit(this.items[0]);
     });
+    this.edit(this.items[0]);
+  }
+
+  add(): void {
+    this.toastr.info('This feature will come soon...');
+  }
+
+  edit(item: Testimonial): void {
+    this.item = item;
+    this.itemFormGroup = this.formBuilder.formGroup(this.item);
+  }
+
+  save(): void {
+    this.appService.saveTestimonial('' + this.item.id, this.item).subscribe((data: any) => {
+      console.log(data);
+      this.toastr.success('Successfully saved...');
+    });
+  }
+
+  private copy(data: any): void {
+    if (data.length === 3) {
+      Object.assign(this.items[0], data[0]);
+      Object.assign(this.items[1], data[1]);
+      Object.assign(this.items[2], data[2]);
+    }
+    if (data.length === 1) {
+      Object.assign(this.items[0], data[0]);
+      Object.assign(this.items[1], data[1]);
+    }
+    if (data.length === 1) {
+      Object.assign(this.items[0], data[0]);
+    }
+  }
+
+  private init(): void {
+    let item = new Testimonial();
+    item.id = 1;
+    this.items.push(item);
+    item = new Testimonial();
+    item.id = 2;
+    this.items.push(item);
+    item = new Testimonial();
+    item.id = 3;
+    this.items.push(item);
   }
 
 }
